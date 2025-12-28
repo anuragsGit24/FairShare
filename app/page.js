dcf4539bb77099492f7713a9d5a59fa9c35d52b9
Anurag <anuragsinghm08@gmail.com>
2025-12-29 01:47:31 +0530

    SetUp Done

---
+++ app/page.js
@@ -0,0 +1,11 @@
+import { Button } from "@/components/ui/button";
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div>Welcome to the FairShare
+      <br/>
+      <Button variant="destructive">Welcome</Button>
+    </div>
+  );
+}
