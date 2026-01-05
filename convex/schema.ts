8ad57771fabac039157c9e1cd25c74bfe4d06892
Anurag <anuragsinghm08@gmail.com>
2026-01-06 00:10:30 +0530

    Auth Done

--- convex/schema.ts
+++ convex/schema.ts
@@ -1,11 +1,11 @@
-import { defineSchema, defineTable } from "convex/server";
-import { v } from "convex/values";
+import { defineSchema, defineTable } from "convex/server"
+import { v } from "convex/values"
 
 export default defineSchema({
-  users: defineTable({
-    name: v.string(),
-    email: v.string(),
-    tokenIdentifier: v.string(),
-    imageUrl: v.optional(v.string()),
-  }),
+    users:defineTable({
+      name: v.string(),
+      email: v.string(),
+      tokenIdentifier: v.string(),
+      imageUrl: v.optional(v.string()),
+    }),
 });
