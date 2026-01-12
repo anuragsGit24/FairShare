d536009207638d231318fda6efe7ad3538aaa68d
Anurag <anuragsinghm08@gmail.com>
2026-01-12 23:58:19 +0530

    Navbar Started

--- components/header.jsx
+++ components/header.jsx
@@ -6,9 +6,11 @@ import Image from "next/image";
 import React from "react";
 import { useStoreUserEffect } from "@/hooks/use-store-user";
 import { BarLoader} from "react-spinners";
+import { usePathname } from "next/navigation";
 
 const Header = () => {
   const { isLoading } = useStoreUserEffect();
+  const path = usePathname();
 
   return (
     <header className="fixed top-0 w-full border-b bg-white/95 backdrop-blur z-50 supports-[backdrop-filter]:bg-white/60">
@@ -23,8 +25,16 @@ const Header = () => {
           />
         </Link>
 
-
-
+        {path==='/' && (
+          <div className="hideen md:flex items-center gap-6">
+            <Link href="#fetures" className="text-sm font-medium hover:text-green-600 transition">
+            Features
+            </Link>
+            <Link href="#fetures" className="text-sm font-medium hover:text-green-600 transition">
+            How It Works
+            </Link>
+          </div>
+        )}
       </nav>
 
       {isLoading && <BarLoader width={"100%"} color="#36d7b7" />}
