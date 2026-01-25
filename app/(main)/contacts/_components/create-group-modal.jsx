dae13d8b383263fcccefeacb71f01f8c1a970a42
Anurag <anuragsinghm08@gmail.com>
2026-01-25 23:59:33 +0530

    Group Modal Started

--- app/(main)/contacts/_components/create-group-modal.jsx
+++ app/(main)/contacts/_components/create-group-modal.jsx
@@ -1,10 +1,21 @@
-import { Dialog } from '@/components/ui/dialog';
+import { Dialog, DialogContent, DialogDescription, DialogFooter, DialogHeader } from '@/components/ui/dialog';
 import React from 'react';
 
-const CreateGroupModal = () => {
+const CreateGroupModal = ({isOpen, isClose, onSuccess}) => {
   return (
     <Dialog>
-      
+      <DialogContent>
+        <DialogHeader>
+          <DialogTitle>Are you absolutely sure?</DialogTitle>
+          <DialogDescription>
+            This action cannot be undone. This will permanently delete your account and remove your data from our
+            servers.
+          </DialogDescription>
+        </DialogHeader>
+        <DialogFooter>
+          Footer
+        </DialogFooter>
+      </DialogContent>
     </Dialog>
   );
 };
